5e312ad46852aadac7bae8e82c232bb6ba8885fd
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2021-07-18 22:00:48 -0400

    Add image search endpoint

--- server/api/main.py
+++ server/api/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 
+from controllers.image_search import get_images
+
 app = FastAPI()
 
 
 @app.get("/")
 def index():
     return "OK"
+
+
+@app.get("/images")
+async def images():
+    results = get_images("monty python and the holy grail")
+
+    return results
